55204db7d41eb5a6afd72393c999822d137ae06d
Paulina Marikin <felicitaspura@gmail.com>
2019-10-04 13:51:50 +0200

    1.2 skończone

--- lab_1/task_2.py
+++ lab_1/task_2.py
@@ -1,5 +1,9 @@
 def task_2():
-    return ''
+    task = '\n'
+    for i in range(1,10):
+        task += '* '*i if i < 6 else '* '*(10-i)
+        task = task[:-1] + '\n'
+    return task
 
 
 assert task_2() == '''
